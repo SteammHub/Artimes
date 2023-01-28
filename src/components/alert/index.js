626cb2fe88c5b28ce3ffc4c15ba8a86083064ca2
SteammHub <123318216+SteammHub@users.noreply.github.com>
2023-01-28 11:12:36 +0300

    Update index.js

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 const Alerts = () => {
     return ( <div className="alert">
-       <a href="https://blog.ipfs.tech/state-of-ipfs-in-js/" class="banner-cta a-alert">
+       <a href="" class="banner-cta a-alert">
         💡 Read more about the state of Artimes in JS in 2022 &amp; 2023
        </a>
     </div> );
